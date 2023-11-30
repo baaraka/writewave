fc23761d1990a5024cde6a5c3104a045809ea835
barakaganai <barakaganai01@gmail.com>
2023-11-30 19:43:53 +0300

    create postList component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import React from "react";
+import Card from "./Card";
+import Pagination from "./Pagination";
 
 const PostList = () => {
   return (
-    <div>
-      <h1>Recent Posts</h1>
+    <div className="flex flex-col gap-5 flex-5">
+      <h1 className="text-3xl font-bold">Recent Posts</h1>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Pagination />
     </div>
   );
 };
