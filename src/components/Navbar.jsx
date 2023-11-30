c86464d02dbd6d016d22a844b685bc3ed5de96d2
barakaganai <barakaganai01@gmail.com>
2023-11-30 19:43:21 +0300

    update navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,28 @@
 import Link from "next/link";
 import React from "react";
+import MenuList from "./MenuList";
 import Menu from "./Menu";
-
-const links = [
-  {
-    id: 1,
-    title: "Home",
-    url: "/",
-  },
-  {
-    id: 2,
-    title: "Portfolio",
-    url: "/portfolio",
-  },
-  {
-    id: 3,
-    title: "Blog",
-    url: "/blog",
-  },
-  {
-    id: 4,
-    title: "About",
-    url: "/about",
-  },
-  {
-    id: 5,
-    title: "Contact",
-    url: "/contact",
-  },
-  {
-    id: 6,
-    title: "Dashboard",
-    url: "/dashboard",
-  },
-];
+import Search from "./Search";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between ">
-      <Link href="/" className="text-3xl md:text-5xl font-bold">
-        WriteWave
-      </Link>
-      <div className="flex items-center gap-5">
-        <div className="hidden lg:flex items-center gap-5">
-          {links.map((link) => (
-            <Link href={link.url} key={link.id} className="text-xl">
-              {link.title}
-            </Link>
-          ))}
-        </div>
-        <div className="lg:hidden">
+    <nav className="flex flex-col gap-10">
+      <div className="flex items-center justify-between">
+        <Link href="/" className="text-3xl md:text-5xl font-bold">
+          WriteWave
+        </Link>
+        <button className="p-4 w-[150px] text-xl bg-green-500 rounded-full">
+          Support Me
+        </button>
+        <button className="p-4 w-[150px] text-xl bg-black rounded-full">
+          Login
+        </button>
+        <div className="md:hidden">
           <Menu />
         </div>
-
-        <button className="hidden md:flex text-xl">Logout</button>
       </div>
+      <MenuList />
+      <Search />
     </nav>
   );
 };
