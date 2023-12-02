81a97962e07dd69217572b9ffaaacc455ce89231
barakaganai <barakaganai01@gmail.com>
2023-12-02 16:57:30 +0300

    update some css

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -5,7 +5,7 @@ import MenuEditor from "./MenuEditor";
 
 const MenuItem = () => {
   return (
-    <div className="flex-2">
+    <div className="hidden md:block flex-2">
       <MenuPost />
       <MenuCategory />
       <MenuEditor />
