6407a42cafd663e15cf2008b7c69ded71dc814a8
barakaganai <barakaganai01@gmail.com>
2023-12-02 16:53:59 +0300

    fetch data in post list

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,18 +2,29 @@ import Image from "next/image";
 import React from "react";
 import Card from "./Card";
 import Pagination from "./Pagination";
+import Button from "./Button";
 
-const PostList = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+};
+
+const PostList = async () => {
+  const posts = await getData();
   return (
     <div className="flex flex-col gap-5 flex-5">
       <h1 className="text-3xl font-bold">Recent Posts</h1>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Pagination />
+
+      {posts.map((post) => (
+        <Card post={post} key={post._id} />
+      ))}
+
+      <Button />
     </div>
   );
 };
