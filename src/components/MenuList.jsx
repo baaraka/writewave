e837ca2650167195ea6a0e2269085ccf5b256590
barakaganai <barakaganai01@gmail.com>
2023-12-04 21:52:04 +0300

    update menu

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -2,47 +2,27 @@ import Link from "next/link";
 import React from "react";
 import Menu from "./Menu";
 
-const links = [
-  {
-    id: 1,
-    title: "Home",
-    url: "/",
-  },
-  {
-    id: 2,
-    title: "Portfolio",
-    url: "/portfolio",
-  },
-  {
-    id: 3,
-    title: "Blog",
-    url: "/blog",
-  },
-  {
-    id: 4,
-    title: "About",
-    url: "/about",
-  },
-  {
-    id: 5,
-    title: "Contact",
-    url: "/contact",
-  },
-  {
-    id: 6,
-    title: "Dashboard",
-    url: "/dashboard",
-  },
-];
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
 
-const MenuList = () => {
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+const MenuList = async () => {
+  const data = await getData();
   return (
-    <div className="hidden md:flex items-center justify-between p-4 bg-gray-800 shadow-lg">
-      {links.map((link) => (
+    <div className="hidden md:flex items-center flex-wrap gap-5 p-4 bg-gray-800 shadow-lg">
+      {data.map((link) => (
         <Link
           href={link.url}
-          key={link.id}
-          className="text-lg hover:underline decoration-green-400 text-center decoration-2 active:underline underline-offset-8 rounded-full hover:font-bold capitalize hover:scale-110 transition-transform duration-200 ease-out"
+          key={link._id}
+          className="text-lg w-[150px] hover:underline decoration-green-400 text-center decoration-2 active:underline underline-offset-8 rounded-full hover:font-bold capitalize hover:scale-110 transition-transform duration-200 ease-out"
         >
           {link.title}
         </Link>
