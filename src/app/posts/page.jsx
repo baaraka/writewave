0d2ad3384a63e41d3c389571f64dd2d40d04f721
barakaganai <barakaganai01@gmail.com>
2023-12-11 23:28:21 +0300

    fetch post of all posts

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -2,16 +2,26 @@ import Card from "@/components/Card";
 import Pagination from "@/components/Pagination";
 import React from "react";
 
-const Posts = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+};
+
+const Posts = async () => {
+  const data = await getData();
   return (
     <div className="my-12 flex flex-col gap-5 flex-5">
       <h1 className="text-3xl font-bold">All Posts</h1>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+
+      {data.map((post) => (
+        <Card post={post} key={post._id} />
+      ))}
+
       <Pagination />
     </div>
   );
