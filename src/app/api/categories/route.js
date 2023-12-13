ece87b46dc650801052e5808fc84657d0193f180
barakaganai <barakaganai01@gmail.com>
2023-12-13 20:26:11 +0300

    update routes

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -3,6 +3,21 @@ import Category from "@/model/Category";
 import { NextResponse } from "next/server";
 
 //create categories
+// export const POST = async (request) => {
+//   const body = await request.json();
+
+//   const newCat = new Category(body);
+
+//   try {
+//     await connect();
+//     await newCat.save();
+
+//     return new NextResponse("Category has been created", { status: 200 });
+//   } catch (error) {
+//     return new NextResponse("Database Error", { status: 500 });
+//   }
+// };
+
 export const POST = async (request) => {
   const body = await request.json();
 
@@ -14,7 +29,17 @@ export const POST = async (request) => {
 
     return new NextResponse("Category has been created", { status: 200 });
   } catch (error) {
-    return new NextResponse("Database Error", { status: 500 });
+    if (error.name === "ValidationError") {
+      // Mongoose validation error
+      const validationErrors = Object.values(error.errors).map(
+        (err) => err.message
+      );
+      return new NextResponse(validationErrors.join(", "), { status: 400 });
+    } else {
+      // Other database error
+      console.error("Database error:", error.message);
+      return new NextResponse("Database error", { status: 500 });
+    }
   }
 };
 
