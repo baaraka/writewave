72240d46069a15db79de328591b3509eff054770
barakaganai <barakaganai01@gmail.com>
2023-12-13 20:26:25 +0300

    handle errors

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,13 +5,17 @@ import Pagination from "./Pagination";
 import Button from "./Button";
 
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/posts", {
-    cache: "no-store",
-  });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+  try {
+    const res = await fetch("http://localhost:3000/api/posts", {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
   }
-  return res.json();
 };
 
 const PostList = async () => {
