6db354a8987779b14a1b6102d488b9fbd37c260a
barakaganai <barakaganai01@gmail.com>
2023-12-15 23:49:32 +0300

    create authProvider

---
+++ src/lib/auth.js
@@ -0,0 +1,48 @@
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connect from "./db";
+import User from "@/model/User";
+import bcrypt from "bcryptjs";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        //Check if the user exists.
+        await connect();
+
+        try {
+          const user = await User.findOne({
+            email: credentials.email,
+          });
+
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error("Wrong Credentials!");
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  pages: {
+    error: "/dashboard/login",
+  },
+};
