e5b663ccdfac8cc4136a8375fca19dea67a12085
barakaganai <barakaganai01@gmail.com>
2023-12-16 23:00:53 +0300

    create delete post endpoint

--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -16,3 +16,17 @@ export const GET = async (request, { params }) => {
     return new NextResponse("Database Error", { status: 500 });
   }
 };
+
+export const DELETE = async (request, { params }) => {
+  const { slug } = params;
+
+  try {
+    await connect();
+
+    await Post.findByIdAndDelete(slug);
+
+    return new NextResponse("Post has been deleted", { status: 200 });
+  } catch (error) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
