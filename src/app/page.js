468d9234b3309d3c28728bf7d99d1baec67844a8
barakaganai <barakaganai01@gmail.com>
2023-12-30 00:54:36 +0300

    update home  page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 import Categories from "@/components/Categories";
-import MenuItem from "@/components/MenuItem";
 import PostList from "@/components/PostList";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,36 +9,9 @@ export default function Home() {
         that make headlines around the world.
       </h1>
 
-      {/* <div className="flex flex-col md:flex-row gap-8 md:gap-12 mt-4">
-        <div className="relative md:flex-1  w-full h-[300px]">
-          <Image
-            src="/culture.png"
-            alt="blog"
-            fill
-            className="rounded-md object-cover"
-          />
-        </div>
-
-        <div className="flex flex-col md:flex-1 gap-5">
-          <h1 className="text-3xl font-bold">
-            Lorem ipsum dolor sit amet consectetur
-          </h1>
-          <p className="text-lg font-light text-[#626262]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-            quasi magni, blanditiis praesentium repellat ducimus eum. Dolore ab
-            ratione suscipit delectus commodi, et, pariatur maxime voluptates
-            laudantium, reiciendis omnis facere.
-          </p>
-          <button className="px-5 py-4 bg-white text-black font-bold rounded-md max-w-max">
-            Read More
-          </button>
-        </div>
-      </div> */}
-
       <Categories />
       <div className="flex gap-12">
         <PostList />
-        <MenuItem />
       </div>
     </main>
   );
