b46060b9ac9bc53d0201f5298d55edd311a8e4be
barakaganai <barakaganai01@gmail.com>
2024-01-06 21:37:34 +0300

    update fetcher

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import Card from "./Card";
 import Button from "./Button";
+import { notFound } from "next/navigation";
 
-const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/posts", {
+const getData = async (category) => {
+  const url = category
+    ? `http://localhost:3000/api/posts?category=${encodeURIComponent(category)}`
+    : "http://localhost:3000/api/posts";
+
+  const res = await fetch(url, {
     cache: "no-store",
   });
   if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    console.error("Error fetching data:", res.status);
+    return notFound();
   }
   return res.json();
 };
 
-const PostList = async () => {
-  const posts = await getData();
+const PostList = async ({ params }) => {
+  const posts = await getData(params.category);
   return (
     <div className="flex flex-col gap-7 flex-8">
       <h1 className="text-3xl font-bold">Recent Posts</h1>
