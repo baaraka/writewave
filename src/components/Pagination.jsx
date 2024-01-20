1f1402f584320727d23bb210052480b16a914002
barakaganai <barakaganai01@gmail.com>
2024-01-20 22:49:05 +0300

    update pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Pagination = () => {
   return (
     <div className="my-12 flex items-center justify-between">
-      <button className="p-2 bg-green-500 text-xl rounded-md">Previous</button>
+      <button className="p-2 bg-green-500 text-xl rounded-md">Prev</button>
       <button className="p-2 bg-green-500 text-xl rounded-md">Next</button>
     </div>
   );
